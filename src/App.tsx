6c537b8631de26dbd5a0627461c6adddcaed65a8
THEBEST7192 <123459429+THEBEST7192@users.noreply.github.com>
2025-09-21 20:48:47 +0200

    Remove text for login and language switcher on phone and instead use bigger icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import './App.css'
 import Header from './UI/Header'
 import MainPage from './MainPage'
-import blueVector from "./assets/blue.svg";
-import yellowVector from "./assets/yellow.svg";
 
 function App() {
 
@@ -11,8 +9,6 @@ function App() {
     <div className="app-container">
       <Header />
       <MainPage />
-      {/* <img src={blueVector} alt="Blue Vector" className="blue-vector" /> */}
-      {/* <img src={yellowVector} alt="Yellow Vector" className="yellow-vector" /> */}
     </div>
   )
 }
